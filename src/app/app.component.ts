87ee4efef3fad694b4cb5395bfecb381f057ea3c
Surya Pavan Anasoori <suryapavan.anasoori@alacriti.com>
2024-03-28 15:06:24 +0530

    Second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,77 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'employee-org';
+
+  myValue=3;
+
+
+  name:string="Surya";
+  isDisabled=false;
+  textId='sampleId';
+
+  show=true;
+
+  src='../favicon.ico';
+  width='200px';
+  // myClass='success height';
+
+  //Array of class
+  myClass={'success': true, 'height': false};
+
+  textStyle='color: red; font-family: cursive;  font-weight:bold';
+
+  employees=['Jhon','Smith','King']
+
+  employee={
+    title:'Software Engineer',
+    name:'Surya Pavan',
+    salary:30000,
+    src:'../favicon.ico',
+    alt:'Profile Pic'
+  }
+
+  getName(data:number):string{
+    if(data==1){
+      return 'Hello From method';
+    }
+    return 'Hello From method value '+data;
+  }
+
+    clickSave(){
+      this.name='Updated' 
+    }
+
+    mouseOver(){
+      this.name='Mouse over';
+    }
+    textChange(){
+      this.name='Changed';
+    }
+
+    //ngFor
+    employeeList=['King','Smith','Rahul']
+
+    employeeDetails=[
+      {name:'Tom', salary:'10000',address:'USA'},
+      {name:'King', salary:'20000',address:'IND'},
+      {name:'Smith', salary:'30000',address:'CHN'},
+      {name:'Tim', salary:'25000',address:'AME'}
+    ]
+
+    refresh(): void{
+      this.employeeDetails=[
+        {name:'Tom', salary:'10000',address:'USA'},
+        {name:'King', salary:'20000',address:'IND'},
+        {name:'Smith', salary:'30000',address:'CHN'},
+        {name:'Tim', salary:'25000',address:'AME'},
+        {name:'S', salary:'25000',address:'AME'},
+        {name:'A', salary:'95000',address:'AME'}
+
+      ]
+    
+    }
+
+    refreshEmployees(index :number ,employee:any): string{
+      return employee.name;
+    }
 }
